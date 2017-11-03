faf5747ca4e2131b619a0296246198f69960b435
Troy Leonard <troyanth@umich.edu>
2017-11-03 18:15:56 -0400

    pass results to twig template for theming

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,21 +13,13 @@ use Drupal\Core\Controller\ControllerBase;
 class DefaultController extends ControllerBase {
 
   public function index($path_id, $query) {
-    $output = "<h1>Search Results for $path_id, $query</h1>";
-
     $response = arborelastic_search($path_id, $query, $_GET);
 
-    $output .= '<p>Search returned ' . $response['hits']['total'] . ' total hits</p>';
-
-    foreach ($response['hits']['hits'] as $hit) {
-      // Theme according to $hit['_type']
-      $output .= '<pre>' . print_r($hit, 1) . '</pre>';
-    }
-
-    return array(
+    return [
       '#title' => 'Search',
-      '#markup' => $output,
-    );
+      '#theme' => 'catalog_results',
+      '#results' => $response
+    ];
   }
 
 }
