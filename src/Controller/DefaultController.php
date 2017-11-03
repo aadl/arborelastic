2ceaf8907cd0761b98a9a4577fd0ee2603d76de5
ejk <eric@rhymeswithtoaster.com>
2017-11-03 18:01:59 -0400

    Add term filters to query

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,7 +15,7 @@ class DefaultController extends ControllerBase {
   public function index($path_id, $query) {
     $output = "<h1>Search Results for $path_id, $query</h1>";
 
-    $response = arborelastic_search($path_id, $query);
+    $response = arborelastic_search($path_id, $query, $_GET);
 
     $output .= '<p>Search returned ' . $response['hits']['total'] . ' total hits</p>';
 
